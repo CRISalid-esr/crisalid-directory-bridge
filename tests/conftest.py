067abbd48aac411aae12b449325c5fb9cea9f5ad
jdp1ps <joachim.dornbusch@univ-paris1.fr>
2024-07-01 13:38:17 +0200

    Enforce dag folder

--- tests/conftest.py
+++ tests/conftest.py
@@ -26,4 +26,10 @@ def fixture_dagbag() -> DagBag:
     Create a DagBag for testing dag loading
     :return: DagBag
     """
-    return DagBag()
+    dag_folder = f"{os.getenv('AIRFLOW_HOME')}/dags"
+    print(f"*********Dag folder: {dag_folder}*********")
+    # print current directory
+    print(f"*********Current file folder: {os.getcwd()}*********")
+    # print directory of this file
+    print(f"*********Current file folder: {os.path.dirname(os.path.abspath(__file__))}*********")
+    return DagBag(dag_folder=dag_folder)
