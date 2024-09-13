fc48e4b97a48571e66a52344630b9506ccefe18d
L-Bara <e-bara@outlook.com>
2024-09-13 11:29:56 +0200

    Export expected results for people in .json

--- tests/spreadsheet/test_convert_spreadsheet_people.py
+++ tests/spreadsheet/test_convert_spreadsheet_people.py
@@ -1,5 +1,5 @@
 import pytest
-
+import json
 from airflow.utils.state import TaskInstanceState
 
 from test_utils.dags import create_dag_run, \
@@ -12,7 +12,7 @@ TEST_TASK_ID = "convert_spreadsheet_people"
 TESTED_TASK_NAME = 'tasks.spreadsheet.convert_spreadsheet_people.convert_spreadsheet_people'
 
 
-@pytest.mark.parametrize("dag, expected_result", [
+@pytest.mark.parametrize("dag, expected_result_path", [
     (
             {
                 "task_name": TESTED_TASK_NAME,
@@ -30,22 +30,7 @@ TESTED_TASK_NAME = 'tasks.spreadsheet.convert_spreadsheet_people.convert_spreads
                     }
                 ]
             },
-            {
-                'uid=jdupond': {
-                    'names': [
-                        {'last_names': [{'value': 'Dupond', 'language': 'fr'}],
-                         'first_names': [{'value': 'Joe', 'language': 'fr'}]}],
-                    'identifiers': [
-                        {'type': 'local', 'value': 'jdupond'},
-                        {'type': 'idhal_i', 'value': ''},
-                        {'type': 'idhal_s', 'value': ''},
-                        {'type': 'orcid', 'value': '0000-0000-0000-0001'},
-                        {'type': 'idref', 'value': '12345678X'},
-                        {'type': 'scopus_eid', 'value': ''},
-                    ],
-                    'memberships': [{'entity': 'U01'}]
-                }
-            }
+            "./tests/data/test_convert_spreadsheet_people.json"
     ),
     (
             {
@@ -64,22 +49,7 @@ TESTED_TASK_NAME = 'tasks.spreadsheet.convert_spreadsheet_people.convert_spreads
                     }
                 ]
             },
-            {
-                'uid=hgerald': {
-                    'names': [
-                        {'last_names': [{'value': 'Gerald', 'language': 'fr'}],
-                         'first_names': [{'value': 'Henry', 'language': 'fr'}]}],
-                    'identifiers': [
-                        {'type': 'local', 'value': 'hgerald'},
-                        {'type': 'idhal_i', 'value': ''},
-                        {'type': 'idhal_s', 'value': ''},
-                        {'type': 'orcid', 'value': ''},
-                        {'type': 'idref', 'value': ''},
-                        {'type': 'scopus_eid', 'value': ''},
-                    ],
-                    'memberships': [{'entity': 'U85'}]
-                }
-            }
+            "./tests/data/test_convert_spreadsheet_people_with_local_identifier_only.json"
     ),
     (
             {
@@ -109,37 +79,7 @@ TESTED_TASK_NAME = 'tasks.spreadsheet.convert_spreadsheet_people.convert_spreads
                     }
                 ]
             },
-            {
-                'uid=hgerald': {
-                    'names': [
-                        {'last_names': [{'value': 'Gerald', 'language': 'fr'}],
-                         'first_names': [{'value': 'Henry', 'language': 'fr'}]}],
-                    'identifiers': [
-                        {'type': 'local', 'value': 'hgerald'},
-                        {'type': 'idhal_i', 'value': '054235'},
-                        {'type': 'idhal_s', 'value': 'henry-gerald'},
-                        {'type': 'orcid', 'value': '0000-0000-0000-0001'},
-                        {'type': 'idref', 'value': '012345678X'},
-                        {'type': 'scopus_eid', 'value': '5432345678X'},
-                    ],
-                    'memberships': [{'entity': 'U85'}]
-                },
-                'uid=jdupond': {
-                    'names': [
-                        {'last_names': [{'value': 'Dupond', 'language': 'fr'}],
-                         'first_names': [{'value': 'Joe', 'language': 'fr'}]}],
-                    'identifiers': [
-                        {'type': 'local', 'value': 'jdupond'},
-                        {'type': 'idhal_i', 'value': ''},
-                        {'type': 'idhal_s', 'value': ''},
-                        {'type': 'orcid', 'value': ''},
-                        {'type': 'idref', 'value': ''},
-                        {'type': 'scopus_eid', 'value': ''},
-                    ],
-                    'memberships': [{'entity': 'U01'}]
-                }
-
-            }
+            "./tests/data/test_convert_spreadsheet_with_two_people.json"
     )
 ],
                          indirect=["dag"],
@@ -149,12 +89,15 @@ TESTED_TASK_NAME = 'tasks.spreadsheet.convert_spreadsheet_people.convert_spreads
                          "test_convert_spreadsheet_with_two_people"
                          ]
                          )
-def test_convert_spreadsheet_people(dag, expected_result, unique_execution_date):
+def test_convert_spreadsheet_people(dag, expected_result_path, unique_execution_date):
     """
     Test that the csv data are converted to the expected format
     """
     dag_run = create_dag_run(dag, DATA_INTERVAL_START, DATA_INTERVAL_END, unique_execution_date)
     ti = create_task_instance(dag, dag_run, TEST_TASK_ID)
     ti.run(ignore_ti_state=True)
+
+    with open(expected_result_path, 'r') as f:
+        expected_result = json.load(f)
     assert ti.state == TaskInstanceState.SUCCESS
     assert ti.xcom_pull(task_ids=TEST_TASK_ID) == expected_result
