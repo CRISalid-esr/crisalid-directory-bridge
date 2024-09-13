1bfa0d0889b0bfc5ee1e06a6b2cf77ffc2f60a3f
L-Bara <e-bara@outlook.com>
2024-09-13 11:40:33 +0200

    Export expected results for structures in .json

--- tests/spreadsheet/test_convert_spreadsheet_structures.py
+++ tests/spreadsheet/test_convert_spreadsheet_structures.py
@@ -1,5 +1,5 @@
 import pytest
-
+import json
 from airflow.utils.state import TaskInstanceState
 
 from test_utils.dags import create_dag_run, \
@@ -12,7 +12,7 @@ TEST_TASK_ID = "convert_spreadsheet_structures"
 TESTED_TASK_NAME = 'tasks.spreadsheet.convert_spreadsheet_structures.convert_spreadsheet_structures'
 
 
-@pytest.mark.parametrize("dag, expected_result", [
+@pytest.mark.parametrize("dag, expected_result_path", [
     (
             {
                 "task_name": TESTED_TASK_NAME,
@@ -30,49 +30,7 @@ TESTED_TASK_NAME = 'tasks.spreadsheet.convert_spreadsheet_structures.convert_spr
                     }
                 ]
             },
-            {
-                'struct_id=U082': {
-                    'names': [
-                        {
-                            'value': 'Laboratoire de géographie physique Pierre Birot (UMR 8591)',
-                            'language': 'fr'
-                        }
-                    ],
-                    'acronym': None,
-                    'descriptions': [
-                        {
-                            'value': 'Lab description',
-                            'language': 'fr'
-                        }
-                    ],
-                    'contacts': [
-                        {
-                            'type': 'postal_address',
-                            'format': 'structured_physical_address',
-                            'value': {
-                                'country': 'France',
-                                'zip_code': '92190',
-                                'city': 'MEUDON',
-                                'street': 'Centre Meudon, 1 PLACE ARISTIDE BRIAND'
-                            }
-                        }
-                    ],
-                    'identifiers': [
-                        {
-                            'type': 'local',
-                            'value': 'U082'
-                        },
-                        {
-                            'type': 'RNSR',
-                            'value': '199812919F'
-                        },
-                        {
-                            'type': 'ROR',
-                            'value': ''
-                        }
-                    ]
-                }
-            }
+            "./tests/data/test_convert_structure_without_acronym.json"
     ),
     (
             {
@@ -91,49 +49,7 @@ TESTED_TASK_NAME = 'tasks.spreadsheet.convert_spreadsheet_structures.convert_spr
                     }
                 ]
             },
-            {
-                'struct_id=U086': {
-                    'names': [
-                        {
-                            'value': 'Laboratoire des Tests (UMR 2024)',
-                            'language': 'fr'
-                        }
-                    ],
-                    'acronym': 'TEST',
-                    'descriptions': [
-                        {
-                            'value': 'Laboratoire des Tests (UMR 2024)',
-                            'language': 'fr'
-                        }
-                    ],
-                    'contacts': [
-                        {
-                            'type': 'postal_address',
-                            'format': 'structured_physical_address',
-                            'value': {
-                                'country': 'France',
-                                'zip_code': '92190',
-                                'city': 'MEUDON',
-                                'street': 'Centre Meudon, 1 PLACE ARISTIDE BRIAND'
-                            }
-                        }
-                    ],
-                    'identifiers': [
-                        {
-                            'type': 'local',
-                            'value': 'U086'
-                        },
-                        {
-                            'type': 'RNSR',
-                            'value': '123456789F'
-                        },
-                        {
-                            'type': 'ROR',
-                            'value': '567890123'
-                        }
-                    ]
-                }
-            }
+            "./tests/data/test_convert_structure_with_acronym.json"
     ),
     (
             {
@@ -163,90 +79,7 @@ TESTED_TASK_NAME = 'tasks.spreadsheet.convert_spreadsheet_structures.convert_spr
                     }
                 ]
             },
-            {
-                'struct_id=U086': {
-                    'names': [
-                        {
-                            'value': 'Laboratoire des Tests (UMR 2024)',
-                            'language': 'fr'
-                        }
-                    ],
-                    'acronym': 'TEST',
-                    'descriptions': [
-                        {
-                            'value': 'Laboratoire des Tests (UMR 2024)',
-                            'language': 'fr'
-                        }
-                    ],
-                    'contacts': [
-                        {
-                            'type': 'postal_address',
-                            'format': 'structured_physical_address',
-                            'value': {
-                                'country': 'France',
-                                'zip_code': '92190',
-                                'city': 'MEUDON',
-                                'street': 'Centre Meudon, 1 PLACE ARISTIDE BRIAND'
-                            }
-                        }
-                    ],
-                    'identifiers': [
-                        {
-                            'type': 'local',
-                            'value': 'U086'
-                        },
-                        {
-                            'type': 'RNSR',
-                            'value': '123456789F'
-                        },
-                        {
-                            'type': 'ROR',
-                            'value': '567890123'
-                        }
-                    ]
-                },
-                'struct_id=U082': {
-                    'names': [
-                        {
-                            'value': 'Laboratoire de géographie physique Pierre Birot (UMR 8591)',
-                            'language': 'fr'
-                        }
-                    ],
-                    'acronym': None,
-                    'descriptions': [
-                        {
-                            'value': 'test geolab',
-                            'language': 'fr'
-                        }
-                    ],
-                    'contacts': [
-                        {
-                            'type': 'postal_address',
-                            'format': 'structured_physical_address',
-                            'value': {
-                                'country': 'France',
-                                'zip_code': '92190',
-                                'city': 'MEUDON',
-                                'street': 'Centre Meudon, 1 PLACE ARISTIDE BRIAND'
-                            }
-                        }
-                    ],
-                    'identifiers': [
-                        {
-                            'type': 'local',
-                            'value': 'U082'
-                        },
-                        {
-                            'type': 'RNSR',
-                            'value': '199812919F'
-                        },
-                        {
-                            'type': 'ROR',
-                            'value': ''
-                        }
-                    ]
-                }
-            }
+            "./tests/data/test_convert_with_two_structures.json"
     ),
 ],
                          indirect=["dag"],
@@ -256,12 +89,14 @@ TESTED_TASK_NAME = 'tasks.spreadsheet.convert_spreadsheet_structures.convert_spr
                              "test_convert_with_two_structures"
                          ]
                          )
-def test_convert_spreadsheet_structures(dag, expected_result, unique_execution_date):
+def test_convert_spreadsheet_structures(dag, expected_result_path, unique_execution_date):
     """
     Test that the csv data are converted to the expected format
     """
     dag_run = create_dag_run(dag, DATA_INTERVAL_START, DATA_INTERVAL_END, unique_execution_date)
     ti = create_task_instance(dag, dag_run, TEST_TASK_ID)
     ti.run(ignore_ti_state=True)
+    with open(expected_result_path, 'r') as f:
+        expected_result = json.load(f)
     assert ti.state == TaskInstanceState.SUCCESS
     assert ti.xcom_pull(task_ids=TEST_TASK_ID) == expected_result
