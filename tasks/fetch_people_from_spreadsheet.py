51cae90b6abf7d45d8da94f42c8315338c010025
L-Bara <e-bara@outlook.com>
2024-09-17 09:49:51 +0200

    Fix identifiers typing issue

--- tasks/fetch_people_from_spreadsheet.py
+++ tasks/fetch_people_from_spreadsheet.py
@@ -2,36 +2,14 @@ import logging
 
 from airflow.decorators import task
 
-from read_spreadsheet import read_spreadsheet
-from utils.config import get_env_variable
+from read_spreadsheet import fetch_people_from_spreadsheet as get_people_rows
 
 logger = logging.getLogger(__name__)
 
 
 @task
 def fetch_people_from_spreadsheet():
-    """Fetch people from a spreadsheet.
 
-    Returns:
-        list: A list of rows from the spreadsheet.
-    """
-
-    columns_to_return = [
-        "first_name",
-        "last_name",
-        "main_laboratory_identifier",
-        "local_identifier",
-        "idHal_i",
-        "idHal_s",
-        "orcid",
-        "idref",
-        "scopus_eid",
-    ]
-
-    data_path = get_env_variable("SPREADSHEET_PEOPLE_PATH")
-
-    df = read_spreadsheet(data_path, columns_to_return)
-
-    rows = df.to_dict(orient='records')
+    rows = get_people_rows()
 
     return rows
--- tasks/fetch_structures_from_spreadsheet.py
+++ tasks/fetch_structures_from_spreadsheet.py
@@ -5,33 +5,14 @@ from airflow.decorators import task
 from read_spreadsheet import read_spreadsheet
 from utils.config import get_env_variable
 
+from read_spreadsheet import fetch_structures_from_spreadsheet as get_structure_rows
+
 logger = logging.getLogger(__name__)
 
 
 @task
 def fetch_structures_from_spreadsheet():
-    """Fetch structure from a spreadsheet.
 
-    Returns:
-        list: A list of rows from the spreadsheet.
-    """
-
-    columns_to_return = [
-        "name",
-        "acronym",
-        "description",
-        "local_identifier",
-        "RNSR",
-        "ROR",
-        "city_name",
-        "city_code",
-        "city_adress",
-    ]
-
-    data_path = get_env_variable("SPREADSHEET_STRUCTURES_PATH")
-
-    df = read_spreadsheet(data_path, columns_to_return)
-
-    rows = df.to_dict(orient='records')
+    rows = get_structure_rows()
 
     return rows
