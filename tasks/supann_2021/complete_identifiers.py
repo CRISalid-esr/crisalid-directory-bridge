dbd5ef1a5200e949070618827dec2e2640781ccf
jdp1ps <joachim.dornbusch@univ-paris1.fr>
2024-09-27 17:41:52 +0200

    Remove void identifiers and skip missing keys in identifiers file

--- tasks/supann_2021/complete_identifiers.py
+++ tasks/supann_2021/complete_identifiers.py
@@ -22,12 +22,18 @@ def complete_identifiers(ldap_source: dict, identifiers_spreadsheet: list[dict[s
         dict: A dict of identifiers with dn as key and a dict with 'identifier'
     """
 
-    identifiers_dict = {f"uid={ident['local']}": [{"type": k, "value": v} for k, v in ident.items()]
+    identifiers_dict = {f"{ident['local']}": [{"type": k, "value": v} for k, v in ident.items()]
                         for ident in identifiers_spreadsheet}
 
     for key, value in ldap_source.items():
+        if key not in identifiers_dict:
+            logger.error("Local identifier %s not found in complementary identifiers", key)
+            continue
+        logger.info("Completing identifiers for %s", key)
         ldap_source[key]['identifiers'] = deduplicate_dict_list(
             value['identifiers'] + identifiers_dict[key])
+        ldap_source[key]['identifiers'] = [ident for ident in ldap_source[key]['identifiers'] if
+                                           ident['value']]
         ldap_source[key]['identifiers'].sort(key=lambda x: x['type'])
     logger.info("Identifiers completed !")
 
