8ec760338eaab1ae27a27cdcb1e93863bbf06a10
L-Bara <e-bara@outlook.com>
2024-10-17 10:55:11 +0200

    Add specific format cases in the converter

--- tasks/supann_2021/convert_ldap_people_employment.py
+++ tasks/supann_2021/convert_ldap_people_employment.py
@@ -9,9 +9,24 @@ from utils.exceptions import YamlParseError
 
 logger = logging.getLogger(__name__)
 
+employee_types_yaml = None # pylint: disable=invalid-name
+
+def _get_employee_types_yaml():
+    global employee_types_yaml
+    if employee_types_yaml is not None:
+        return employee_types_yaml
+    yaml_path = get_env_variable('YAML_EMPLOYEE_TYPE_PATH')
+    try:
+        with open(yaml_path, 'r', encoding='UTF-8') as file:
+            employee_types_yaml = yaml.safe_load(file)
+    except (FileNotFoundError, ValueError, yaml.YAMLError) as error:
+        raise YamlParseError(f"Error while reading the yaml: {str(error)}") from error
+
+    return employee_types_yaml
+
 
 def _get_position_code_and_label(
-        employee_type_to_check: str,
+        employee_type_to_check: str | None,
         field_type: str
 ) -> tuple[str, str] | None:
     """
@@ -22,15 +37,9 @@ def _get_position_code_and_label(
         tuple[str, str] | None: A tuple with the code and the label of the position if the
         employee type is known. Else None
     """
-    yaml_path = get_env_variable('YAML_EMPLOYEE_TYPE_PATH')
-    try:
-        with open(yaml_path, 'r', encoding='UTF-8') as file:
-            employee_types_yaml = yaml.safe_load(file)
-    except (FileNotFoundError, ValueError, yaml.YAMLError) as error:
-        raise YamlParseError(f"Error while reading the yaml: {str(error)}") from error
 
     position = next(
-        ((code, inner_dict['label']) for code, inner_dict in employee_types_yaml.items()
+        ((code, inner_dict['label']) for code, inner_dict in _get_employee_types_yaml().items()
          if employee_type_to_check in inner_dict[field_type]),
         None
     )
@@ -59,48 +68,49 @@ def _create_employment(
 
 
 def _convert_with_supanempprofil(
-        list_dict_profil: list[dict]
+        profil_dict_list: list[dict]
 ) -> list[dict]:
     employments = []
-    for profil in list_dict_profil:
-        entity_uid = f"uai-{profil['etab'].removeprefix('{UAI}')}"
-        position = None
-        corps = profil.get('corps', None)
-        if corps:
-            corps = profil['corps'].removeprefix('{NCORPS}')
-            position = _get_position_code_and_label(corps, 'corps')
-        employments.append(_create_employment(entity_uid, position))
+    for profil in profil_dict_list:
+        if isinstance(profil['etab'], str) and profil['etab'].startswith('{UAI}'):
+            entity_uid = f"uai-{profil['etab'].removeprefix('{UAI}')}"
+            position = None
+            corps = profil.get('corps', None)
+            if isinstance(corps, str) and corps.startswith('{NCORPS}'):
+                corps = profil['corps'].removeprefix('{NCORPS}')
+                position = _get_position_code_and_label(corps, 'corps')
+            employments.append(_create_employment(entity_uid, position))
     return employments
 
 
 def _convert_with_employeetype(
-        employee_entry: dict[list[str], list[str]]
+        employee_entry: dict[(str, list[str]), (str, list[str])]
 ) -> list[dict]:
     employments = []
-    related_establishment = employee_entry.get('supannEtablissement', [])
-    establishment_count = len(related_establishment)
-    employee_type = employee_entry.get('employeeType', [])
-    employee_type_count = len(employee_type)
+    establishments = employee_entry.get('supannEtablissement', [])
+    establishment_count = len(establishments)
+    if establishment_count == 0:
+        return employments
+    employee_types = employee_entry.get('employeeType', [])
+    employee_type_count = len(employee_types)
 
     if establishment_count > employee_type_count:
-        employee_type += [None] * (establishment_count - employee_type_count)
+        employee_types += [None] * (establishment_count - employee_type_count)
 
-    if establishment_count == 1 < employee_type_count:
-        related_establishment.extend(
-            [related_establishment[0]] * (employee_type_count - establishment_count))
-
-    # NOTE:If we have multiple establishments and multiple type,
-    # but we have more type than establishments, we can't rely on the lists to make correspondances
-    if 1 < establishment_count < employee_type_count:
-        employee_type = [None] * establishment_count
+    if establishment_count < employee_type_count:
+        if establishment_count == 1:
+            establishments.extend(
+                [establishments[0]] * (employee_type_count - establishment_count))
+        else:
+            employee_types = [None] * establishment_count
 
     formatted_establishment = [
         f"uai-{match.group(1)}"
-        for item in related_establishment
+        for item in establishments
         # Regular expression match example: {UAI}0000000Z
         if (match := re.search(r'^\{UAI\}(\d{7}[A-Z])$', item))
     ]
-    for entity_uid, employee_type_to_check in zip(formatted_establishment, employee_type):
+    for entity_uid, employee_type_to_check in zip(formatted_establishment, employee_types):
         position = _get_position_code_and_label(employee_type_to_check, 'local_values')
         employments.append(_create_employment(entity_uid, position))
     return employments
@@ -109,7 +119,7 @@ def _convert_with_employeetype(
 @task(task_id="convert_ldap_people_employment")
 def convert_ldap_people_employment(
         ldap_results: dict[str, dict[str, str | dict]]
-) -> dict[dict[list[dict[str, str | dict]]]]:
+) -> dict[str, dict[str, list[dict]]]:
     """
     Args:
         ldap_results (dict): A dict of ldap results with dn as key and entry as value
@@ -120,12 +130,12 @@ def convert_ldap_people_employment(
 
     for dn, entry in ldap_results.items():
         emp_profil = entry.get('supannEmpProfil', [])
-        list_dict_profil = [
+        profil_dict_list = [
             dict(item.split('=') for item in profil.strip('[]').split(']['))
             for profil in emp_profil
         ]
-        if len(list_dict_profil) > 0:
-            employments = _convert_with_supanempprofil(list_dict_profil)
+        if len(profil_dict_list) > 0:
+            employments = _convert_with_supanempprofil(profil_dict_list)
         else:
             employments = _convert_with_employeetype(entry)
 
--- tests/tasks/supann_2021/test_convert_ldap_people_employment.py
+++ tests/tasks/supann_2021/test_convert_ldap_people_employment.py
@@ -255,6 +255,53 @@ def test_case_with_one_entity_and_two_known_affectation(dag, unique_execution_da
         "param_names": ["raw_results"],
         "raw_results": {
             'uid=hdupont,ou=people,dc=univ-paris1,dc=fr': {
+                'employeeType': [
+                    "Chargé d'enseignement",
+                    'Maître de conférences',
+                    "Conservateur general des bibliothèques"
+                ],
+                'supannEtablissement': [
+                    '{UAI}0753364Z',
+                    '{UAI}0258465Z'
+                ],
+            },
+        },
+    }
+], indirect=True)
+def test_case_with_multiples_entities_and_more_known_affectation(dag, unique_execution_date):
+    """
+    Test that the employment are converted from the LDAP entry when 2 employeeType are known for 1
+    supannEtablissement
+    :param dag: The DAG object
+    :param unique_execution_date: unique execution date
+    :return: None
+    """
+    dag_run = create_dag_run(dag, DATA_INTERVAL_START, DATA_INTERVAL_END, unique_execution_date)
+    ti = create_task_instance(dag, dag_run, TEST_TASK_ID)
+    ti.run(ignore_ti_state=True)
+    assert ti.state == TaskInstanceState.SUCCESS
+    assert ti.xcom_pull(task_ids=TEST_TASK_ID) == {
+        'uid=hdupont,ou=people,dc=univ-paris1,dc=fr': {
+            "employments": [
+                {
+                    "position": {},
+                    "entity_uid": "uai-0753364Z"
+                },
+                {
+                    "position": {},
+                    "entity_uid": "uai-0258465Z"
+                }
+            ]
+        }
+    }
+
+
+@pytest.mark.parametrize("dag", [
+    {
+        "task_name": TESTED_TASK_NAME,
+        "param_names": ["raw_results"],
+        "raw_results": {
+            'uid=hdupont,ou=people,dc=univ-paris1,dc=fr': {
                 'supannEmpProfil': [
                     '[etab={UAI}0753364Z][affil=faculty][corps={NCORPS}300][typeaffect={SUPANN}S108][affect=COV1][population={SUPANN}RGPF]'
                 ]
@@ -325,6 +372,75 @@ def test_case_with_two_supannempprofil(dag, unique_execution_date):
                         "title": "Professeur des universités",
                         "code": "pr"
                     },
+                    "entity_uid": "uai-0753364Z"
+                }
+            ]
+        }
+    }
+
+
+@pytest.mark.parametrize("dag", [
+    {
+        "task_name": TESTED_TASK_NAME,
+        "param_names": ["raw_results"],
+        "raw_results": {
+            'uid=hdupont,ou=people,dc=univ-paris1,dc=fr': {
+                'supannEmpProfil': [
+                    '[etab={OTHER}0753364Z][affil=faculty][corps={NCORPS}300][typeaffect={'
+                    'SUPANN}S108][affect=COV1][population={SUPANN}RGPF]'
+                ]
+            },
+        },
+    }
+], indirect=True)
+def test_case_with_supannempprofil_unexpected_etab_value(dag, unique_execution_date):
+    """
+    Test that the employment are converted from the LDAP entry when a supannEmpProfil is available.
+    :param dag: The DAG object
+    :param unique_execution_date: unique execution date
+    :return: None
+    """
+    dag_run = create_dag_run(dag, DATA_INTERVAL_START, DATA_INTERVAL_END, unique_execution_date)
+    ti = create_task_instance(dag, dag_run, TEST_TASK_ID)
+    ti.run(ignore_ti_state=True)
+    assert ti.state == TaskInstanceState.SUCCESS
+    assert ti.xcom_pull(task_ids=TEST_TASK_ID) == {
+        'uid=hdupont,ou=people,dc=univ-paris1,dc=fr': {
+            "employments": []
+        }
+    }
+
+
+@pytest.mark.parametrize("dag", [
+    {
+        "task_name": TESTED_TASK_NAME,
+        "param_names": ["raw_results"],
+        "raw_results": {
+            'uid=hdupont,ou=people,dc=univ-paris1,dc=fr': {
+                'supannEmpProfil': [
+                    '[etab={UAI}0753364Z][affil=faculty][corps={OTHER}300][typeaffect={'
+                    'SUPANN}S108][affect=COV1][population={SUPANN}RGPF]'
+                ]
+            },
+        },
+    }
+], indirect=True)
+def test_case_with_supannempprofil_unexpected_corps_value(dag, unique_execution_date):
+    """
+    Test that the employment are converted from the LDAP entry when a supannEmpProfil is available.
+    :param dag: The DAG object
+    :param unique_execution_date: unique execution date
+    :return: None
+    """
+    dag_run = create_dag_run(dag, DATA_INTERVAL_START, DATA_INTERVAL_END, unique_execution_date)
+    ti = create_task_instance(dag, dag_run, TEST_TASK_ID)
+    ti.run(ignore_ti_state=True)
+    assert ti.state == TaskInstanceState.SUCCESS
+    assert ti.xcom_pull(task_ids=TEST_TASK_ID) == {
+        'uid=hdupont,ou=people,dc=univ-paris1,dc=fr': {
+            "employments": [
+                {
+                    "position": {},
                     "entity_uid": "uai-0753364Z"
                 }
             ]
