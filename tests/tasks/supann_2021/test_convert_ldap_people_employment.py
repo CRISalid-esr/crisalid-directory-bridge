1bb62e9e325aa5bc3f1b00673fca5172ce1b448e
jdp1ps <joachim.dornbusch@univ-paris1.fr>
2025-02-25 10:31:35 +0100

    Fix tests

--- tests/tasks/supann_2021/test_convert_ldap_people_employment.py
+++ tests/tasks/supann_2021/test_convert_ldap_people_employment.py
@@ -44,7 +44,8 @@ def test_usual_case(dag, unique_execution_date):
                         "title": "Professeur",
                         "code": "PR"
                     },
-                    "entity_uid": "uai-0753364Z"
+                    "entity_uid": "0753364Z",
+                    'entity_uid_type': 'UAI',
                 }
             ]
         }
@@ -83,14 +84,16 @@ def test_case_with_multiple_affectations_in_different_entities(dag, unique_execu
                         "title": "Professeur",
                         "code": "PR"
                     },
-                    "entity_uid": "uai-0753364Z"
+                    "entity_uid": "0753364Z",
+                    'entity_uid_type': 'UAI',
                 },
                 {
                     "position": {
                         "title": "Directeur de recherche et assimilés",
                         "code": "DR"
                     },
-                    "entity_uid": "uai-0258465Z"
+                    "entity_uid": "0258465Z",
+                    'entity_uid_type': 'UAI',
                 }
             ]
         }
@@ -159,11 +162,13 @@ def test_case_with_two_different_entities_and_one_known_affectation(dag, unique_
                         "title": "Professeur",
                         "code": "PR"
                     },
-                    "entity_uid": "uai-0753364Z"
+                    "entity_uid": "0753364Z",
+                    'entity_uid_type': 'UAI',
                 },
                 {
                     "position": {},
-                    "entity_uid": "uai-0258465Z"
+                    "entity_uid": "0258465Z",
+                    'entity_uid_type': 'UAI',
                 }
             ]
         }
@@ -207,14 +212,16 @@ def test_case_with_one_entity_and_two_known_affectation(dag, unique_execution_da
                         "title": "Professeur certifié",
                         "code": "PCAP"
                     },
-                    "entity_uid": "uai-0753364Z"
+                    "entity_uid": "0753364Z",
+                    'entity_uid_type': 'UAI',
                 },
                 {
                     "position": {
                         "title": "Maître de conférences",
                         "code": "MCF"
                     },
-                    "entity_uid": "uai-0753364Z"
+                    "entity_uid": "0753364Z",
+                    'entity_uid_type': 'UAI',
                 }
             ]
         }
@@ -257,11 +264,13 @@ def test_case_with_multiples_entities_and_more_known_affectation(dag, unique_exe
             "employments": [
                 {
                     "position": {},
-                    "entity_uid": "uai-0753364Z"
+                    "entity_uid": "0753364Z",
+                    'entity_uid_type': 'UAI',
                 },
                 {
                     "position": {},
-                    "entity_uid": "uai-0258465Z"
+                    "entity_uid": "0258465Z",
+                    'entity_uid_type': 'UAI',
                 }
             ]
         }
