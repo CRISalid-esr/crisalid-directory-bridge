f712fbf3deb0c2fd09831fbe54ba4c0bf6ceae88
jdp1ps <116366856+jdp1ps@users.noreply.github.com>
2025-03-07 13:34:30 +0100

    60 handle institution identifiers both as uais and as rors (#62)
    * Update employment transcoding mechanism
    
    * Add missing employment types for P1PS LDAP
    
    * Set UAI identifier code to uppercase

--- tests/tasks/supann_2021/test_convert_ldap_people_employment.py
+++ tests/tasks/supann_2021/test_convert_ldap_people_employment.py
@@ -44,7 +44,7 @@ def test_usual_case(dag, unique_execution_date):
                         "title": "Professeur",
                         "code": "PR"
                     },
-                    "entity_uid": "uai-0753364Z"
+                    "entity_uid": "UAI-0753364Z",
                 }
             ]
         }
@@ -83,14 +83,14 @@ def test_case_with_multiple_affectations_in_different_entities(dag, unique_execu
                         "title": "Professeur",
                         "code": "PR"
                     },
-                    "entity_uid": "uai-0753364Z"
+                    "entity_uid": "UAI-0753364Z",
                 },
                 {
                     "position": {
                         "title": "Directeur de recherche et assimilés",
                         "code": "DR"
                     },
-                    "entity_uid": "uai-0258465Z"
+                    "entity_uid": "UAI-0258465Z",
                 }
             ]
         }
@@ -159,11 +159,11 @@ def test_case_with_two_different_entities_and_one_known_affectation(dag, unique_
                         "title": "Professeur",
                         "code": "PR"
                     },
-                    "entity_uid": "uai-0753364Z"
+                    "entity_uid": "UAI-0753364Z",
                 },
                 {
                     "position": {},
-                    "entity_uid": "uai-0258465Z"
+                    "entity_uid": "UAI-0258465Z",
                 }
             ]
         }
@@ -207,14 +207,14 @@ def test_case_with_one_entity_and_two_known_affectation(dag, unique_execution_da
                         "title": "Professeur certifié",
                         "code": "PCAP"
                     },
-                    "entity_uid": "uai-0753364Z"
+                    "entity_uid": "UAI-0753364Z",
                 },
                 {
                     "position": {
                         "title": "Maître de conférences",
                         "code": "MCF"
                     },
-                    "entity_uid": "uai-0753364Z"
+                    "entity_uid": "UAI-0753364Z",
                 }
             ]
         }
@@ -257,11 +257,11 @@ def test_case_with_multiples_entities_and_more_known_affectation(dag, unique_exe
             "employments": [
                 {
                     "position": {},
-                    "entity_uid": "uai-0753364Z"
+                    "entity_uid": "UAI-0753364Z",
                 },
                 {
                     "position": {},
-                    "entity_uid": "uai-0258465Z"
+                    "entity_uid": "UAI-0258465Z",
                 }
             ]
         }
