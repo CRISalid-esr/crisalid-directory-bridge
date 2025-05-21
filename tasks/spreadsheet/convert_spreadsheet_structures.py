31391ceded3a5845312f672b3b205ad357e2c23d
bp-IEpro <benoit.pandolfi@univ-paris1.fr>
2025-05-21 15:45:58 +0200

    refactor: update identifier handling and adjust related data and tests (#76)
    * refactor: update identifier handling and adjust related data and tests
    
    - Renamed 'rnsr' identifiers with 'nns' in input processing logic
    - Added support for the 'scopus_id' identifier
    - Added support for the 'collection_hal' identifier
    - Updated `structures.csv` files accordingly in both the main data and test fixtures
    - Modified tests to reflect the identifier changes and ensure correctness with the updated schema
    
    * refactor: update identifier handling and adjust related data and tests
    
    - Renamed 'rnsr' identifiers with 'nns' in input processing logic
    - Added support for the 'scopus_id' identifier
    - Added support for the 'collection_hal' identifier
    - Updated `structures.csv` files accordingly in both the main data and test fixtures
    - Modified tests to reflect the identifier changes and ensure correctness with the updated schema

--- tasks/spreadsheet/convert_spreadsheet_structures.py
+++ tasks/spreadsheet/convert_spreadsheet_structures.py
@@ -6,11 +6,12 @@ logger = logging.getLogger(__name__)
 
 LOCAL_STRUCTURE_IDENTIFIER = 'tracking_id'
 
-STRUCTURE_IDENTIFIERS = [LOCAL_STRUCTURE_IDENTIFIER, 'rnsr', 'ror']
+STRUCTURE_IDENTIFIERS = [LOCAL_STRUCTURE_IDENTIFIER, 'nns', 'ror', 'scopus_id', 'hal_collection']
 
 # Mapping of identifiers to their standardized type names
 IDENTIFIER_TYPE_MAP = {
     'tracking_id': 'local',
+    'hal_collection': 'hal'
 }
 
 
