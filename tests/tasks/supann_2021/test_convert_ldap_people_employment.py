a76dce594f281fec59610ccf2f9f2303d71224c4
jdp1ps <joachim.dornbusch@univ-paris1.fr>
2026-02-17 14:48:28 +0100

    Fix UAI identifier case issue

--- tests/tasks/supann_2021/test_convert_ldap_people_employment.py
+++ tests/tasks/supann_2021/test_convert_ldap_people_employment.py
@@ -49,7 +49,7 @@ def test_usual_case(dag, unique_logical_date):
                         "title": "Professeur",
                         "code": "PR"
                     },
-                    "entity_uid": "UAI-0753364Z",
+                    "entity_uid": "uai-0753364Z",
                 }
             ]
         }
@@ -93,14 +93,14 @@ def test_case_with_multiple_affectations_in_different_entities(dag, unique_logic
                         "title": "Professeur",
                         "code": "PR"
                     },
-                    "entity_uid": "UAI-0753364Z",
+                    "entity_uid": "uai-0753364Z",
                 },
                 {
                     "position": {
                         "title": "Directeur de recherche et assimilés",
                         "code": "DR"
                     },
-                    "entity_uid": "UAI-0258465Z",
+                    "entity_uid": "uai-0258465Z",
                 }
             ]
         }
@@ -171,11 +171,11 @@ def test_case_with_two_different_entities_and_one_known_affectation(dag, unique_
                         "title": "Professeur",
                         "code": "PR"
                     },
-                    "entity_uid": "UAI-0753364Z",
+                    "entity_uid": "uai-0753364Z",
                 },
                 {
                     "position": {},
-                    "entity_uid": "UAI-0258465Z",
+                    "entity_uid": "uai-0258465Z",
                 }
             ]
         }
@@ -222,14 +222,14 @@ def test_case_with_one_entity_and_two_known_affectation(dag, unique_logical_date
                         "title": "Professeur certifié",
                         "code": "PCAP"
                     },
-                    "entity_uid": "UAI-0753364Z",
+                    "entity_uid": "uai-0753364Z",
                 },
                 {
                     "position": {
                         "title": "Maître de conférences",
                         "code": "MCF"
                     },
-                    "entity_uid": "UAI-0753364Z",
+                    "entity_uid": "uai-0753364Z",
                 }
             ]
         }
@@ -273,11 +273,11 @@ def test_case_with_multiples_entities_and_more_known_affectation(dag, unique_log
             "employments": [
                 {
                     "position": {},
-                    "entity_uid": "UAI-0753364Z",
+                    "entity_uid": "uai-0753364Z",
                 },
                 {
                     "position": {},
-                    "entity_uid": "UAI-0258465Z",
+                    "entity_uid": "uai-0258465Z",
                 }
             ]
         }
@@ -316,7 +316,7 @@ def test_unknown_employee_type_logs_warning(dag, unique_logical_date, caplog):
             "employments": [
                 {
                     "position": {},
-                    "entity_uid": "UAI-0753364Z",
+                    "entity_uid": "uai-0753364Z",
                 }
             ]
         }
@@ -355,7 +355,7 @@ def test_unknown_employee_type_logs_warning(dag, unique_logical_date, caplog):
                 "employments": [
                     {
                         "position": {},
-                        "entity_uid": "UAI-0753364Z",
+                        "entity_uid": "uai-0753364Z",
                     }
                 ]
             }
